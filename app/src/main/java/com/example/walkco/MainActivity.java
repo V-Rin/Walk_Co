f18a5d7fb71096924d2228424d57749d17a898aa
V-Rin <rena.grena@yandex.ru>
2022-09-26 07:27:53 +0300

    Project was created

---
+++ app/src/main/java/com/example/walkco/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.walkco;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navFragment);
+        NavController navController = navHostFragment.getNavController();
+        NavigationView toolbar = findViewById(R.id.navigation);
+        NavigationUI.setupWithNavController(toolbar, navController);
+    }
+}
